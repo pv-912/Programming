f965a9843ba8390f18764f8f8cea7eef05669545
9prashant-1223 <prashantverma1223@gmail.com>
2018-01-01 22:37:19 +0530

    merge sort

--- C++ Files/merge_sort_me.cpp
+++ C++ Files/merge_sort_me.cpp
@@ -7,8 +7,8 @@ void merge(int A[],int p,int q,int r){
 	int n1 = q-p+1;
 	int n2 = r-q; 
 
-	int a[n1+1];
-	int b[n2+1];
+	int a[n1];
+	int b[n2];
 
 	for( int i=0; i<n1; i++){
 		a[i] = A[p+i];
@@ -18,12 +18,13 @@ void merge(int A[],int p,int q,int r){
 		b[j] = A[q+j+1];
 	}
 
-	a[n1+1] = 'a';  //why to add one more
-	b[n2+1] = 'a';	// why to add one more
+	// a[n1+1] = 'a';  //why to add one more
+	// b[n2+1] = 'a';	// why to add one more
 
 	int i = 0;
 	int j = 0;
-	for(int k=p;k<=r;k++){
+	int k = p;
+	for(k;k<r;k++){
 		if(a[i]<=b[j]){
 			A[k] = a[i];
 			i++;
@@ -33,13 +34,22 @@ void merge(int A[],int p,int q,int r){
 		}
 	}
 
-	
-	
+	while(i<n1){
+		A[k] = a[i];
+		i++;
+		k++;
+	}
+
+	while(j<n2){
+		A[k] = a[j];
+		j++;
+		k++;
+	}	
 }
 
 void merge_sort(int A[],int p, int r){
 	if(p<r){
-		int q = (p+r)/2;
+		int q = (p+(r-1))/2;
 		merge_sort(A,p,q);
 		merge_sort(A,q+1,r);
 		merge(A,p,q,r);
@@ -48,11 +58,16 @@ void merge_sort(int A[],int p, int r){
 
 int main(){
 
-	int A[20] = {5,8,4,6,9,1,7,5,7,2,4,5,7,1,2,3,6,5,8,9};
-	merge_sort(A,9,16);
+	int A[] = {10,9,8,7,6,5,4,3,2,1,0};
+	int size = sizeof(A)/sizeof(A[0]);
+	for(int i = 0;i<size;i++){
+		cout<<A[i]<<" ";
+	}
+	cout<<endl;
+	merge_sort(A,0,size);
 
-	for(int i = 0;i<20;i++){
-		cout<<A[i]<<endl;
+	for(int i = 0;i<size;i++){
+		cout<<A[i]<<" ";
 	}
 
 	return 0;
