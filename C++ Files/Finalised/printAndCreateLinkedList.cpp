ac4f2af393e18331cb801a1cad10007d9ac40874
9prashant-1223 <prashantverma1223@gmail.com>
2018-03-23 11:19:26 +0530

    delete and length of linked list

--- C++ Files/Finalised/printAndCreateLinkedList.cpp
+++ C++ Files/Finalised/printAndCreateLinkedList.cpp
@@ -41,11 +41,65 @@ void pushLast(struct node** headRef, int newData){
 	return;
 }
 
+void deleteNode(struct node** headRef, int key){
+	struct node* temp = *headRef, *prev;
+
+	if(temp!=NULL && temp->data==key){
+		*headRef = temp->next;
+		free(temp);
+		return; 
+	} 
+
+	while(temp!=NULL && temp->data!=key){
+		prev = temp;
+		temp = temp->next;
+	}
+	prev->next = temp->next;
+	free(temp);
+}
+
+void deleteNodeAtGivenPosition(struct node** headRef, int position){
+	struct node* temp = *headRef;
+
+	if(*headRef==NULL){
+		return;
+	}
+
+
+	if(position==0){
+		*headRef = temp->next;
+		free(temp);
+		return;
+	}
+
+	for(int i=0;temp!=0 && i<position-1; i++){
+		temp = temp->next;
+	}
+
+	if(temp==0 || temp->next==0){
+			return;
+	}
+	// struct node *next = temp->next->next;
+	free(temp->next);
+	temp->next = temp->next->next;
+}
+
+void listLength(struct node** headRef){
+	struct node* temp = *headRef;
+	int a = 0;
+	while(temp!=NULL){
+		temp = temp->next;
+		a++;
+	}
+	cout<<a<<endl;
+}
+
 void printList(node *a){
     while(a != NULL){
         cout<<a->data<<" ";
         a = a -> next;
     }
+    cout<<endl;
 }
 
 int main() {
@@ -69,8 +123,15 @@ int main() {
 	pushHead(&first, 5);
 	pushBetween(first->next, 6);
 	pushLast(&first,9);
+	listLength(&first);
+	printList(first);
 
+	deleteNode(&first, 3);
+	printList(first);
+
+	deleteNodeAtGivenPosition(&first, 1);
+	deleteNodeAtGivenPosition(&first, 3);
 	printList(first);
 	return 0;
-	
+
 }
